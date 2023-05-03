8bd18dbb7dec15963f450ec8b350db720a4af2f4
atravieso <angeltraviesoc@gmail.com>
2023-05-03 16:29:28 -0400

    Modo observador - Watch mode

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,13 @@
 "use strict";
+// Modo observador
+// tsc --w
+// tsc -w
 const msg = 'Hola mundo';
+const hero = {
+    name: 'Ironman',
+    age: 45,
+};
+// hero.age = '50'; ❌
+hero.age = 55; // ✔
 console.log(msg);
+console.log(hero);
--- bases/app.ts
+++ bases/app.ts
@@ -1,3 +1,17 @@
+// Modo observador
+// tsc --w
+// tsc -w
+
 const msg:string = 'Hola mundo';
 
+const hero = {
+    name: 'Ironman',
+    age: 45,
+}
+
+// hero.age = '50'; ❌
+
+hero.age = 55; // ✔
+
 console.log(msg);
+console.log(hero);
